64f3b3e70f4f7e75cb0291d84a882cc2cb736e19
vic.zeng <vic.zeng@idreamsky.com>
2022-06-17 14:51:47 +0800

    [2022-06-17 14:51:41] commit: update1

---
+++ push.bat
@@ -0,0 +1,14 @@
+@echo off
+set d=%date:~0,10%
+set t=%time:~0,8%
+set timestamp=%d% %t%
+ 
+set /p comments=please input commit commits:
+echo [%timestamp%] comments: %comments% >> history.txt
+ 
+git add .
+git commit -m "[%timestamp%] commit: %comments%"
+git push
+git log --stat -1
+echo "Finished Push!"
+pause
