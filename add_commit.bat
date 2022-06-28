da783343f1783a75fd6aa523669ec5d1ed7bb8bf
vic.zeng <vic.zeng@idreamsky.com>
2022-06-29 07:57:29 +0800

    [2022-06-29  7:57:25] commit: add v4

--- add_commit.bat
+++ add_commit.bat
@@ -11,11 +11,11 @@ for /f  %%i in (%filePath%) do (
 
  
 set /p comments=please input commit commits:
-echo [%timestamp%] commit: %comments% %lastline% >> history.txt
+echo [%timestamp%] commit: %comments% v%lastline% >> history.txt
 echo %lastLine% >> ver.txt
  
 git add .
-git commit -m "[%timestamp%] commit: %comments%"
+git commit -m "[%timestamp%] commit: %comments% v%lastline%"
 
 git log --stat -1
 echo "Finished Push!"
