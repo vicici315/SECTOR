bf5b13a58ea9b13b5a1023c6f6402dcec35cf77d
vic.zeng <vic.zeng@idreamsky.com>
2023-08-24 16:43:42 +0800

    [2023-Aug-24 Thursday 16:43:42] SQL_1.8完善版本 (viczeng) v19

--- DataSearch.bat
+++ DataSearch.bat
@@ -6,13 +6,13 @@ rem 在输入的版本号减0.1
 for /f %%A in ('powershell -command "[math]::Round(%comments% - 0.1, 2)"') do set "result=%%A"
 
 rem 删除文件和文件夹
-del /q %~n0_%result%.spec
-RD /Q /S build\%~n0_%result%\
+del /q %~n0-SQL_%result%.spec
+RD /Q /S build\%~n0-SQL_%result%\
 
 call %~dp0venv\Scripts\activate
 
 chcp 65001
-%~dp0venv\Scripts\pyinstaller %~dp0%~n0.py -w -F --icon=%~n0.ico -n=%~n0_%comments%
+%~dp0venv\Scripts\pyinstaller %~dp0%~n0.py -w -F --icon=%~n0.ico -n=%~n0-SQL_%comments%
 pause
 
 ::pyinstaller d:\Gits\MyWinUI\DataSearch.py -w -F --icon=d:\Gits\MyWinUI\DataSearch.ico -n=DataSearch
