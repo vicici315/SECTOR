65cf155c883c80c38266d1c8b5d491c626780251
vic.zeng <vic.zeng@idreamsky.com>
2023-09-02 00:43:06 +0800

    [2023-Sep-02 Saturday 00:43:06] v12完善功能；添加版本更新机制 (viczeng) v28

--- DataSearch.bat
+++ DataSearch.bat
@@ -3,8 +3,8 @@ echo off
 set /p comments= 编译版本: 
 
 rem 在输入的版本号减0.1
-for /f %%A in ('powershell -command "[math]::Round(%comments% - 0.1, 2)"') do set "result=%%A"
-
+REM for /f %%A in ('powershell -command "[math]::Round(%comments% - 0.1, 2)"') do set "result=%%A"
+set /a "result=comments - 1"
 rem 删除文件和文件夹
 del /q %~n0_%result%.spec
 RD /Q /S build\%~n0_%result%\
@@ -16,3 +16,4 @@ chcp 65001
 pause
 
 ::pyinstaller d:\Gits\MyWinUI\DataSearch.py -w -F --icon=d:\Gits\MyWinUI\DataSearch.ico -n=DataSearch
+
