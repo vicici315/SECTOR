8e4d08a5ec87bf50b82bdd238920b44e1911b8df
vic.zeng <vic.zeng@idreamsky.com>
2023-10-13 00:52:35 +0800

    v17_优化数据库，用户可以创建自己的数据库设置密码，取消0号数据库显，添加第二个数据库链接setr专用于设置的数据库链接线 (viczeng) v42

--- DCupdate.bat
+++ DCupdate.bat
@@ -2,7 +2,7 @@
 
 taskkill /f /im DupliceClicker.exe
 
-del /f %~d0%~p0DupliceClicker.exe
+REM del /f %~d0%~p0DupliceClicker.exe
 timeout /nobreak /t 2
 copy /y "\\192.168.10.38\点睛工作室\Softs\DUPCLICKER\DupliceClicker.exe" %~d0%~p0DupliceClicker.exe
 copy /y "\\192.168.10.38\点睛工作室\Softs\DUPCLICKER\DupliceClicker,Desktop.bat" "%~d0%~p0DupliceClicker,Desktop.bat"
